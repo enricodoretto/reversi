ef2a67559078cffdf26f035db903b1f2eb848171
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-18 16:44:29 +0100

    We can create a disk and check its color persists if no moves are done

--- src/test/java/sdm/reversi/TestDisk.java
+++ src/test/java/sdm/reversi/TestDisk.java
@@ -1,9 +1,19 @@
 package sdm.reversi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDisk {
+
+    @ParameterizedTest
+    @EnumSource(Disk.Color.class)
+    public void hasOriginalSideUpWithNoMoves(Disk.Color color){
+        Disk disk = new Disk(color);
+        assertEquals(color, disk.getSideUp());
+    }
 
     @Test
     public void hasWhiteSideUpAfterCreatedWhiteWithNoMoves(){
