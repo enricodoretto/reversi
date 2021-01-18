df61698de8fa7cef117d0d5967a173b71b9551ed
Davide Michielin <davidemichie@gmail.com>
2021-01-18 18:12:45 +0100

    Delete duplicated test in TestCoordinate

--- src/test/java/sdm/reversi/TestCoordinate.java
+++ src/test/java/sdm/reversi/TestCoordinate.java
@@ -18,32 +18,5 @@ public class TestCoordinate {
         );
     }
 
-    @Test
-    void hasRow1AndColumnAAsCreated() {
-        Coordinate coordinate = new Coordinate(1, 'A');
-        assertAll(
-                () -> assertEquals(1, coordinate.getRow()),
-                () -> assertEquals('A', coordinate.getColumn())
-        );
-    }
-
-    @Test
-    void hasRow2AndColumnDAsCreated() {
-        Coordinate coordinate = new Coordinate(2, 'D');
-        assertAll(
-                () -> assertEquals(2, coordinate.getRow()),
-                () -> assertEquals('D', coordinate.getColumn())
-        );
-    }
-
-    @Test
-    void hasRow3AndColumnCAsCreated() {
-        Coordinate coordinate = new Coordinate(3, 'C');
-        assertAll(
-                () -> assertEquals(3, coordinate.getRow()),
-                () -> assertEquals('C', coordinate.getColumn())
-        );
-    }
-
 
 }
