00fb7db6013ff6ee80e8564e793ab57e4a2b847f
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-18 16:45:44 +0100

    Duplicate tests removed in TestDisk

--- src/test/java/sdm/reversi/TestDisk.java
+++ src/test/java/sdm/reversi/TestDisk.java
@@ -15,16 +15,4 @@ public class TestDisk {
         assertEquals(color, disk.getSideUp());
     }
 
-    @Test
-    public void hasWhiteSideUpAfterCreatedWhiteWithNoMoves(){
-        Disk whiteDisk = new Disk(Disk.Color.WHITE);
-        assertEquals(Disk.Color.WHITE, whiteDisk.getSideUp());
-    }
-
-    @Test
-    public void hasBlackSideUpAfterCreatedBlackWithNoMoves(){
-        Disk blackDisk = new Disk(Disk.Color.BLACK);
-        assertEquals(Disk.Color.BLACK, blackDisk.getSideUp());
-    }
-
 }
