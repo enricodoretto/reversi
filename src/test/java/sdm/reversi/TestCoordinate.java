30e8d477a86ac3ee6b10b88708be3ab4c8ed6370
Davide Michielin <davidemichie@gmail.com>
2021-01-18 18:12:09 +0100

    We can create coordinate with every row and column

--- src/test/java/sdm/reversi/TestCoordinate.java
+++ src/test/java/sdm/reversi/TestCoordinate.java
@@ -1,11 +1,23 @@
 package sdm.reversi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCoordinate {
+    @ParameterizedTest
+    @CsvSource({"1, A", "2, D", "3, C"})
+    public void hasSameRowAndColumnAsCreated(int row, char column){
+        Coordinate coordinate = new Coordinate(row, column);
+        assertAll(
+                () -> assertEquals(row, coordinate.getRow()),
+                () -> assertEquals(column, coordinate.getColumn())
+        );
+    }
+
     @Test
     void hasRow1AndColumnAAsCreated() {
         Coordinate coordinate = new Coordinate(1, 'A');
