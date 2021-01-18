c5f398bec708f5f4475e23620861b4a052b0b9a6
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-18 16:25:36 +0100

    We can create a black disk and check its color persists if no moves are done

--- src/test/java/sdm/reversi/TestDisk.java
+++ src/test/java/sdm/reversi/TestDisk.java
@@ -11,4 +11,10 @@ public class TestDisk {
         assertEquals(Disk.Color.WHITE, whiteDisk.getSideUp());
     }
 
+    @Test
+    public void hasBlackSideUpAfterCreatedBlackWithNoMoves(){
+        Disk blackDisk = new Disk(Disk.Color.BLACK);
+        assertEquals(Disk.Color.BLACK, blackDisk.getSideUp());
+    }
+
 }
