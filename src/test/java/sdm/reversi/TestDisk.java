c27bc77672f94087e95f2d6f4108d6f75743c39c
Enrico Doretto <enricodoretto@MBP-di-Enrico-2.lan>
2021-01-18 17:20:12 +0100

    Parametrized tests for flipping disks

--- src/test/java/sdm/reversi/TestDisk.java
+++ src/test/java/sdm/reversi/TestDisk.java
@@ -2,6 +2,7 @@ package sdm.reversi;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,6 +14,14 @@ public class TestDisk {
     public void hasOriginalSideUpWithNoMoves(Disk.Color color){
         Disk disk = new Disk(color);
         assertEquals(color, disk.getSideUp());
+    }
+
+    @ParameterizedTest
+    @CsvSource({"BLACK, WHITE", "WHITE, BLACK"})
+    public void flipDisk(Disk.Color initialColor, Disk.Color flippedColor){
+        Disk disk = new Disk(initialColor);
+        disk.flip();
+        assertEquals(flippedColor, disk.getSideUp());
     }
 
     @Test
