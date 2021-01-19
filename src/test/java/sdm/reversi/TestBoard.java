80fe01d21fdf7a330aba11e373398b55935afb73
Enrico Doretto <enricodoretto@MBP-di-Enrico-2.lan>
2021-01-19 17:48:14 +0100

    We grouped the tests for putting the disks in an empty cell of the board

--- src/test/java/sdm/reversi/TestBoard.java
+++ src/test/java/sdm/reversi/TestBoard.java
@@ -1,10 +1,21 @@
 package sdm.reversi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestBoard {
+
+    @ParameterizedTest
+    @CsvSource({"1, A", "2, A", "5, C"})
+    public void putDiskInEmptyCell(int row, char column){
+        Board board = new Board();
+        Disk disk = new Disk(Disk.Color.BLACK);
+        assertTrue(board.putDisk(disk, row, column));
+    }
 
     @Test
     public void putDiskIn1AEmptyCell(){
