382b9a10b8e8108e67058e4071e8689ea5a1c60f
Davide Michielin <davidemichie@gmail.com>
2021-01-20 10:30:37 +0100

    We can now create a game with two players

--- src/test/java/sdm/reversi/TestGame.java
+++ src/test/java/sdm/reversi/TestGame.java
@@ -1,11 +1,24 @@
 package sdm.reversi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestGame {
+    @ParameterizedTest
+    @CsvSource({"Bob, Alice", "Jack, John", "Simon, Leonard"})
+    void createGameWithTwoPlayers(String player1Name, String player2Name) {
+        Game game = new Game(player1Name, player2Name);
+        assertAll(
+                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), game.getPlayer1()),
+                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), game.getPlayer2())
+        );
+    }
+
     @Test
     void createGameWithPlayerBobWithBlackDiskAndPlayerAliceWithWhiteDisk() {
         Game game = new Game("Bob", "Alice");
