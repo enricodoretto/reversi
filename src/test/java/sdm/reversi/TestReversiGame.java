709f3e15294642a6f5af905ab1b02ea982b5363e
Adriano Tumino <adrianotumino@gmail.com>
2021-01-22 22:59:23 +0100

    Refactor gameWithFullWhiteBoardIsOver in TestReversiGame: now it use putDisk that takes with dickColor in input

--- src/test/java/sdm/reversi/TestReversiGame.java
+++ src/test/java/sdm/reversi/TestReversiGame.java
@@ -172,7 +172,7 @@ public class TestReversiGame {
         Board board = new Board();
         for(int row=0; row<8; row++){
             for(int column=0; column<8; column++){
-                board.putDisk(new Disk(Disk.Color.WHITE), new Coordinate(row,column));
+                board.putDisk(Disk.Color.WHITE, new Coordinate(row,column));
             }
         }
         Game game = new ReversiGame("Bob", "Alice", board);
