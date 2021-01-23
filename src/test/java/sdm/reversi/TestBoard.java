9a04ea5fc63afc3c6232ed98babf2e9d7242af71
Adriano Tumino <adrianotumino@gmail.com>
2021-01-23 09:13:13 +0100

    Refactor TestBoard test: now putDiskInEmptyCell uses the constructor and not the parseCoordinate function

--- src/test/java/sdm/reversi/TestBoard.java
+++ src/test/java/sdm/reversi/TestBoard.java
@@ -19,7 +19,7 @@ public class TestBoard {
     @CsvSource({"1A", "2A", "5C"})
     public void putDiskInEmptyCell(String stringCoordinate){
         Board board = new Board();
-        Coordinate coordinate = Coordinate.parseCoordinate(stringCoordinate);
+        Coordinate coordinate = new Coordinate(stringCoordinate);
         assertTrue(board.putDisk(Disk.Color.BLACK, coordinate));
     }
 
