0debf42632025a4ec30805a0d062994a772a01e0
enricodoretto <e.doretto@gmail.com>
2021-01-25 16:31:34 +0100

    Refactored Board: inlined isCellEmpty

--- src/main/java/sdm/reversi/Board.java
+++ src/main/java/sdm/reversi/Board.java
@@ -19,6 +19,70 @@ public class Board implements Iterable<Coordinate> {
         board = new Disk[size][size];
     }
 
+    public boolean isCellEmpty(Coordinate coordinate) {
+        int row = coordinate.getRow();
+        int column = coordinate.getColumn();
+        if (!isValidIndex(row) || !isValidIndex(column)) {
+            throw new IllegalArgumentException();
+        }
+        return board[row][column] == null;
+    }
+
+    private boolean isValidIndex(int index) {
+        return index >= 0 && index <= size - 1;
+    }
+
+    public boolean isValidCell(Coordinate coordinate) {
+        return !isValidIndex(coordinate.getRow()) || !isValidIndex(coordinate.getColumn());
+    }
+
+    public boolean putDisk(Disk.Color diskColor, Coordinate coordinate){
+        if (!isValidIndex(coordinate.getRow()) || !isValidIndex(coordinate.getColumn()))
+            throw new IllegalArgumentException();
+        if (isCellEmpty(coordinate)) {
+            board[coordinate.getRow()][coordinate.getColumn()] = new Disk(diskColor);
+            return true;
+        }
+        return false;
+    }
+
+    public void flipDisk(Coordinate coordinate) {
+        if (!isValidIndex(coordinate.getRow()) || !isValidIndex(coordinate.getColumn()))
+            throw new IllegalArgumentException();
+        board[coordinate.getRow()][coordinate.getColumn()].flip();
+    }
+
+    public Disk.Color getDiskColorFromCoordinate(Coordinate coordinate) {
+        Disk disk = board[coordinate.getRow()][coordinate.getColumn()];
+        if (disk == null) {
+            return null;
+        }
+        return disk.getSideUp();
+    }
+
+
+    @Override
+    public String toString() {
+        return Arrays.stream(board).map(row -> Arrays.toString(row)
+                .replace("null","-")
+                .replaceAll("\\[|\\]|,", "")
+                .replace(" ", "")).collect(Collectors.joining("\n"));
+    }
+
+    public Disk.Color getColorWithMoreDisks() {
+        Map<Disk.Color, Integer> diskColorCounters = new HashMap<>();
+        diskColorCounters.put(Disk.Color.BLACK, 0);
+        diskColorCounters.put(Disk.Color.WHITE, 0);
+        for (Coordinate coordinate : this) {
+            if (board[coordinate.getRow()][coordinate.getColumn()] == null) {
+                continue;
+            }
+            diskColorCounters.put(board[coordinate.getRow()][coordinate.getColumn()].getSideUp(),
+                    diskColorCounters.get(board[coordinate.getRow()][coordinate.getColumn()].getSideUp()) + 1);
+        }
+        return Collections.max(diskColorCounters.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+
     public Iterator<Coordinate> iterator() {
         return new Iterator<>() {
 
@@ -49,69 +113,4 @@ public class Board implements Iterable<Coordinate> {
         };
     }
 
-    public boolean putDisk(Disk.Color diskColor, Coordinate coordinate){
-        if (!isValidIndex(coordinate.getRow()) || !isValidIndex(coordinate.getColumn()))
-            throw new IllegalArgumentException();
-        if (isCellEmpty(coordinate)) {
-            board[coordinate.getRow()][coordinate.getColumn()] = new Disk(diskColor);
-            return true;
-        }
-        return false;
-    }
-
-    public void flipDisk(Coordinate coordinate) {
-        if (!isValidIndex(coordinate.getRow()) || !isValidIndex(coordinate.getColumn()))
-            throw new IllegalArgumentException();
-        board[coordinate.getRow()][coordinate.getColumn()].flip();
-    }
-
-    public Disk.Color getDiskColorFromCoordinate(Coordinate coordinate) {
-        Disk disk = board[coordinate.getRow()][coordinate.getColumn()];
-        if (disk == null) {
-            return null;
-        }
-        return disk.getSideUp();
-    }
-
-    public boolean isCellEmpty(Coordinate coordinate) {
-        return isCellEmpty(coordinate.getRow(), coordinate.getColumn());
-    }
-
-    private boolean isCellEmpty(int row, int column) {
-        if (isValidIndex(row) && isValidIndex(column)) {
-            return board[row][column] == null;
-        } else {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private boolean isValidIndex(int index) {
-        return index >= 0 && index <= size - 1;
-    }
-
-    public boolean isValidCell(Coordinate coordinate) {
-        return !isValidIndex(coordinate.getRow()) || !isValidIndex(coordinate.getColumn());
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.stream(board).map(row -> Arrays.toString(row)
-                .replace("null","-")
-                .replaceAll("\\[|\\]|,", "")
-                .replace(" ", "")).collect(Collectors.joining("\n"));
-    }
-
-    public Disk.Color getColorWithMoreDisks() {
-        Map<Disk.Color, Integer> diskColorCounters = new HashMap<>();
-        diskColorCounters.put(Disk.Color.BLACK, 0);
-        diskColorCounters.put(Disk.Color.WHITE, 0);
-        for (Coordinate coordinate : this) {
-            if (board[coordinate.getRow()][coordinate.getColumn()] == null) {
-                continue;
-            }
-            diskColorCounters.put(board[coordinate.getRow()][coordinate.getColumn()].getSideUp(),
-                    diskColorCounters.get(board[coordinate.getRow()][coordinate.getColumn()].getSideUp()) + 1);
-        }
-        return Collections.max(diskColorCounters.entrySet(), Map.Entry.comparingByValue()).getKey();
-    }
 }
