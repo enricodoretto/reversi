96c7843392b8ca7fdcc4500149be6a20961264c2
enricodoretto <e.doretto@gmail.com>
2021-01-25 12:38:29 +0100

    Removed duplicated tests with delegated disk creation in TestBoardIsRepresented

--- src/test/java/sdm/reversi/TestBoardIsRepresented.java
+++ src/test/java/sdm/reversi/TestBoardIsRepresented.java
@@ -8,79 +8,50 @@ public class TestBoardIsRepresented {
 
     @Test
     public void emptyBoardRepresentedAsString(){
-        String emptyBoard = "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------";
+        String emptyBoard = """
+                --------
+                --------
+                --------
+                --------
+                --------
+                --------
+                --------
+                --------""";
         Board board = new Board();
         assertEquals(emptyBoard, board.toString());
     }
 
     @Test
     public void boardWithWhiteDiskInCell1ARepresentedAsString(){
-        String emptyBoard = "W-------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------";
+        String emptyBoard = """
+                W-------
+                --------
+                --------
+                --------
+                --------
+                --------
+                --------
+                --------""";
         Board board = new Board();
         board.putDisk(Disk.Color.WHITE,new Coordinate("1A"));
-        assertEquals(emptyBoard, board.toString());
-    }
-
-    @Test
-    public void boardWithWhiteDiskInCell1ARepresentedAsStringDelegatingDiskCreation(){
-        String emptyBoard = "W-------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------";
-        Board board = new Board();
-        board.putDisk(Disk.Color.WHITE, new Coordinate("1A"));
-        assertEquals(emptyBoard, board.toString());
-    }
-
-    @Test
-    public void boardWithWhiteDiskInCell1AAndBlackDiskInCell8HRepresentedAsStringDelegatingDiskCreation(){
-        String emptyBoard = "W-------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "-------B";
-        Board board = new Board();
-        board.putDisk(Disk.Color.WHITE,new Coordinate("1A"));
-        board.putDisk(Disk.Color.BLACK,new Coordinate("8H"));
         assertEquals(emptyBoard, board.toString());
     }
 
     @Test
     public void boardWithWhiteDiskInCell1AAndBlackDiskInCell8HRepresentedAsString(){
-        String emptyBoard = "W-------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "--------\n" +
-                "-------B";
+        String emptyBoard = """
+                W-------
+                --------
+                --------
+                --------
+                --------
+                --------
+                --------
+                -------B""";
         Board board = new Board();
         board.putDisk(Disk.Color.WHITE,new Coordinate("1A"));
         board.putDisk(Disk.Color.BLACK,new Coordinate("8H"));
         assertEquals(emptyBoard, board.toString());
     }
-
 
 }
