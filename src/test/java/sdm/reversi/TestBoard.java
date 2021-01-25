1ea9576549e516c270335c47046d1146ef2604af
enricodoretto <e.doretto@gmail.com>
2021-01-25 09:30:52 +0100

    Refactor TestBoard: we now let coordinate be parsed implicitly

--- src/test/java/sdm/reversi/TestBoard.java
+++ src/test/java/sdm/reversi/TestBoard.java
@@ -10,59 +10,53 @@ public class TestBoard {
 
     @ParameterizedTest
     @CsvSource({"0A", "2Z", "10L"})
-    public void checkingIfEmptyCellOutsideBoardIsNotPossible(String stringCoordinate){
+    public void checkingIfEmptyCellOutsideBoardIsNotPossible(Coordinate coordinate){
         Board board = new Board();
-        assertThrows(IllegalArgumentException.class, () -> board.isCellEmpty(new Coordinate(stringCoordinate)));
+        assertThrows(IllegalArgumentException.class, () -> board.isCellEmpty(coordinate));
     }
 
     @ParameterizedTest
     @CsvSource({"1A", "2A", "5C"})
-    public void putDiskInEmptyCell(String stringCoordinate){
+    public void putDiskInEmptyCell(Coordinate coordinate){
         Board board = new Board();
-        Coordinate coordinate = new Coordinate(stringCoordinate);
         assertTrue(board.putDisk(Disk.Color.BLACK, coordinate));
     }
 
     @ParameterizedTest
     @CsvSource({"1A", "2A", "5C"})
-    public void putDiskInEmptyCellDelegatingDiskCreation(String stringCoordinate){
+    public void putDiskInEmptyCellDelegatingDiskCreation(Coordinate coordinate){
         Board board = new Board();
-        Coordinate coordinate = new Coordinate(stringCoordinate);
         assertTrue(board.putDisk(Disk.Color.BLACK, coordinate));
     }
 
 
     @ParameterizedTest
     @CsvSource({"1A", "5F", "8H"})
-    public void cantPutDiskInNonEmptyCell(String stringCoordinate){
+    public void cantPutDiskInNonEmptyCell(Coordinate coordinate){
         Board board = new Board();
-        Coordinate coordinate = new Coordinate(stringCoordinate);
         board.putDisk(Disk.Color.BLACK,coordinate);
         assertFalse(board.putDisk(Disk.Color.BLACK, coordinate));
     }
 
     @ParameterizedTest
     @CsvSource({"1A", "5F", "8H"})
-    public void cantPutDiskInNonEmptyCellDelegatingDiskCreation(String stringCoordinate){
+    public void cantPutDiskInNonEmptyCellDelegatingDiskCreation(Coordinate coordinate){
         Board board = new Board();
-        Coordinate coordinate = new Coordinate(stringCoordinate);
         board.putDisk(Disk.Color.BLACK,coordinate);
         assertFalse(board.putDisk(Disk.Color.BLACK, coordinate));
     }
 
     @ParameterizedTest
     @CsvSource({"11C", "0A", "4I"})
-    public void cantPutDiskInCellOutOfBoard(String stringCoordinate){
+    public void cantPutDiskInCellOutOfBoard(Coordinate coordinate){
         Board board = new Board();
-        Coordinate coordinate = new Coordinate(stringCoordinate);
         assertThrows(IllegalArgumentException.class, () -> board.putDisk(Disk.Color.BLACK,coordinate));
     }
 
     @ParameterizedTest
     @CsvSource({"11C", "0A", "4I"})
-    public void cantPutDiskInCellOutOfBoardDelegatingDiskCreation(String stringCoordinate){
+    public void cantPutDiskInCellOutOfBoardDelegatingDiskCreation(Coordinate coordinate){
         Board board = new Board();
-        Coordinate coordinate = new Coordinate(stringCoordinate);
         assertThrows(IllegalArgumentException.class, () -> board.putDisk(Disk.Color.BLACK,coordinate));
     }
 
