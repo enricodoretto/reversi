2d5a25e694a153d0ab9a4937c942096f34acae10
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-26 10:54:19 +0100

    TestCreateReversiGame class created and test for creating reversi game with two players with different names and default board moved there

---
+++ src/test/java/sdm/reversi/TestCreateReversiGame.java
@@ -0,0 +1,21 @@
+package sdm.reversi;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCreateReversiGame {
+
+    @ParameterizedTest
+    @CsvSource({"Bob, Alice", "Jack, John", "Simon, Leonard"})
+    void withTwoPlayersWithDifferentNamesAndDefaultBoardSucceeds(String player1Name, String player2Name) {
+        Game game = new ReversiGame(player1Name, player2Name);
+        assertAll(
+                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), game.getPlayer1()),
+                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), game.getPlayer2())
+        );
+    }
+
+}
--- src/test/java/sdm/reversi/TestReversiGame.java
+++ src/test/java/sdm/reversi/TestReversiGame.java
@@ -15,15 +15,7 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestReversiGame {
-    @ParameterizedTest
-    @CsvSource({"Bob, Alice", "Jack, John", "Simon, Leonard"})
-    void createGameWithTwoPlayers(String player1Name, String player2Name) {
-        Game game = new ReversiGame(player1Name, player2Name);
-        assertAll(
-                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), game.getPlayer1()),
-                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), game.getPlayer2())
-        );
-    }
+
 
     @ParameterizedTest
     @CsvSource({"Bob", "Alice", "John"})
