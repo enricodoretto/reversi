5fbaa7cd4cf8c6345f9a6d516ec22f18e6a0766e
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-26 17:58:07 +0100

    TestCreateOthelloGame renamed to TestOthelloGameCreation

--- src/test/java/sdm/reversi/TestOthelloGameCreation.java
+++ src/test/java/sdm/reversi/TestOthelloGameCreation.java
@@ -11,7 +11,7 @@ import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TestCreateOthelloGame {
+public class TestOthelloGameCreation {
 
     @ParameterizedTest
     @CsvSource({"Bob, Alice", "Jack, John", "Simon, Leonard"})
