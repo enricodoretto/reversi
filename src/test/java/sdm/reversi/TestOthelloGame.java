ded6adcf180e0b5d4324bf9ae6eb01aae98b3177
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-26 12:51:10 +0100

    Improved TestOthelloGame method to check a game is over when both players are in stall

--- src/test/java/sdm/reversi/TestOthelloGame.java
+++ src/test/java/sdm/reversi/TestOthelloGame.java
@@ -21,6 +21,14 @@ public class TestOthelloGame {
     }
 
     @ParameterizedTest
+    @CsvSource("othello2011FinalBoard, othello2017FinalBoard")
+    void withBothPlayersInStallIsOver(String boardFileName) throws IOException {
+        URL boardFile = TestBoardIsRepresented.class.getClassLoader().getResource(boardFileName);
+        Game game = new OthelloGame("Bob", "Alice", boardFile);
+        assertTrue(game.isOver());
+    }
+
+    @ParameterizedTest
     @CsvSource("allWhite8x8Board, allBlack8x8Board, first4RowsWhiteAndLast4RowsBlack8x8Board")
     void withFullBoardIsOver(String boardFileName) throws IOException {
         URL boardFile = TestBoardIsRepresented.class.getClassLoader().getResource(boardFileName);
@@ -37,17 +45,11 @@ public class TestOthelloGame {
     }
 
     @Test
-    void gameWith2017FinalBoardIsOver() throws IOException {
-        URL boardFile = TestBoardIsRepresented.class.getClassLoader().getResource("othello2017FinalBoard");
-        Game game = new OthelloGame("Bob", "Alice", boardFile);
-        assertTrue(game.isOver());
-    }
-
-    @Test
     void gameWith2017FinalBoardIsWonByAlice() throws IOException {
         URL boardFile = TestBoardIsRepresented.class.getClassLoader().getResource("othello2017FinalBoard");
         Game game = new OthelloGame("Bob", "Alice", boardFile);
         assertEquals("Alice", game.getWinner().getName());
     }
+
 
 }
