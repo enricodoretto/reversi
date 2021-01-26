426ee92762161699c63e8b38242d1f943c8e560a
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-26 09:29:55 +0100

    Move class improved adding constructor, a getter and overriding equals

--- src/main/java/sdm/reversi/Move.java
+++ src/main/java/sdm/reversi/Move.java
@@ -1,12 +1,32 @@
 package sdm.reversi;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class Move {
 
-    private Coordinate diskPosition;
-    private Set<Coordinate> coordinatesOfDisksToFlip;
+    private final Coordinate diskPosition;
+    private final Set<Coordinate> coordinatesOfDisksToFlip;
 
+    public Move(Coordinate diskPosition, Set<Coordinate> coordinatesOfDisksToFlip) {
+        this.diskPosition = diskPosition;
+        this.coordinatesOfDisksToFlip = coordinatesOfDisksToFlip;
+    }
 
+    public Set<Coordinate> getCoordinatesOfDisksToFlip() {
+        return coordinatesOfDisksToFlip;
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Move move = (Move) o;
+        return Objects.equals(diskPosition, move.diskPosition);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(diskPosition);
+    }
 }
