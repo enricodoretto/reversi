1c65399863bcc648e5d3da0af8857496e20175fe
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-27 10:51:09 +0100

    We can now check that if a player writes an invalid move he needs to write another one

--- src/test/java/sdm/reversi/TestOthelloPlay.java
+++ src/test/java/sdm/reversi/TestOthelloPlay.java
@@ -10,10 +10,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestOthelloPlay {
 
     @Test
-    void IsOverAfterTwoMoves() throws IOException {
+    void isOverAfterTwoMoves() throws IOException {
         URL boardFile = TestBoardIsRepresented.class.getClassLoader().getResource("othello4x4BoardTwoMovesMissing");
         Game game = new OthelloGame("Bob", "Alice", boardFile);
         String str = "4C" + System.lineSeparator() + "4D" + System.lineSeparator();
+        ByteArrayInputStream bais = new ByteArrayInputStream(str.getBytes());
+        System.setIn(bais);
+        assertEquals("Bob", game.play().getName());
+    }
+
+    @Test
+    void requiresMoveRepetitionIfGivenMoveIsInvalid() throws IOException {
+        URL boardFile = TestBoardIsRepresented.class.getClassLoader().getResource("othello4x4BoardTwoMovesMissing");
+        Game game = new OthelloGame("Bob", "Alice", boardFile);
+        String str = "4C" + System.lineSeparator() + "4C" + System.lineSeparator() + "4D" + System.lineSeparator();
         ByteArrayInputStream bais = new ByteArrayInputStream(str.getBytes());
         System.setIn(bais);
         assertEquals("Bob", game.play().getName());
