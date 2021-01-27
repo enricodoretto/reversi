5eaf92ed05371db04fe9fd55fb5adb96ebd0bf0c
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-01-27 16:02:54 +0100

    Overridden calculatePlayerPossibleMoves in Reversi to take into account different game beginning

--- src/main/java/sdm/reversi/game/ReversiGame.java
+++ src/main/java/sdm/reversi/game/ReversiGame.java
@@ -1,9 +1,22 @@
 package sdm.reversi.game;
 
+import sdm.reversi.Coordinate;
+import sdm.reversi.Disk;
+
 import java.io.IOException;
 import java.net.URL;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ReversiGame extends Game {
+
+    Coordinate[] initialMoves = new Coordinate[]{
+            new Coordinate(board.getSize() / 2 - 1, board.getSize() / 2 - 1),
+            new Coordinate(board.getSize() / 2 , board.getSize() / 2 - 1),
+            new Coordinate(board.getSize() / 2 - 1, board.getSize() / 2 ),
+            new Coordinate(board.getSize() / 2 , board.getSize() / 2)};
 
     public ReversiGame(String player1Name, String player2Name) throws IllegalArgumentException {
         super(player1Name, player2Name);
@@ -21,6 +34,23 @@ public class ReversiGame extends Game {
         super(player1Name, player2Name, boardSize);
         calculatePlayerPossibleMoves();
         updatePlayersScore();
+    }
+
+    @Override
+    protected void calculatePlayerPossibleMoves(){
+        if(numberOfMoves>=4){
+            super.calculatePlayerPossibleMoves();
+            return;
+        }
+        /*Map<Coordinate, Set<Coordinate>> validCoordinates = Arrays.stream(initialMoves)
+                .filter(c -> board.isCellAvailable(c))
+                .collect(Collectors.toMap(Function.identity(), new HashSet<>()));*/
+        allowedMovesForCurrentPlayer = new HashMap<>();
+        for(Coordinate coordinate : initialMoves){
+            if(board.isCellAvailable(coordinate)){
+                allowedMovesForCurrentPlayer.put(coordinate, new HashSet<>());
+            }
+        }
     }
 
     @Override
