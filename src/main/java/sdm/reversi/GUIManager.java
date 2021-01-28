55a06a9f7ea824391e50bd8c1e52f35a095ebc44
enricodoretto <e.doretto@gmail.com>
2021-01-28 15:32:12 +0100

    Created GUIManager class

---
+++ src/main/java/sdm/reversi/GUIManager.java
@@ -0,0 +1,31 @@
+package sdm.reversi;
+
+import sdm.reversi.game.Game;
+
+public class GUIManager implements IOManager{
+
+    @Override
+    public void updateGame(Game game) {
+
+    }
+
+    @Override
+    public Coordinate getMoveFromPlayer() {
+        return null;
+    }
+
+    @Override
+    public void startTurn(String message) {
+
+    }
+
+    @Override
+    public void illegalMove(String message) {
+
+    }
+
+    @Override
+    public void initialize(Game game) {
+
+    }
+}
