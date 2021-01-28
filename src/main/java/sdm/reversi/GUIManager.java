4a2030099c94e1ab9e418a4d03800bf991d1bd73
enricodoretto <e.doretto@gmail.com>
2021-01-28 15:42:36 +0100

    Added fields to GUIManager

--- src/main/java/sdm/reversi/GUIManager.java
+++ src/main/java/sdm/reversi/GUIManager.java
@@ -2,7 +2,22 @@ package sdm.reversi;
 
 import sdm.reversi.game.Game;
 
-public class GUIManager implements IOManager{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class GUIManager extends JFrame implements IOManager, ActionListener {
+    private JLabel player1Name, player2Name, player1Score, player2Score, currentPlayerName;
+    private JPanel[][] graphicBoard;
+    private final static int FRAME_SIZE = 700;
+    private int diskRadius;
+    //private boolean[][] diskIsPresent;
+    //private int numberOfMoves = 0;
+    private ArrayList<Point> points;
+    //private boolean currentColor; //check color of player
+
 
     @Override
     public void updateGame(Game game) {
@@ -26,6 +41,11 @@ public class GUIManager implements IOManager{
 
     @Override
     public void initialize(Game game) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
