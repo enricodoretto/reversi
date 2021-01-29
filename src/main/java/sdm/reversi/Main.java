e02c2f6268a6877bc5ee2da965df26b4d2d5fafe
enricodoretto <e.doretto@gmail.com>
2021-01-29 16:47:07 +0100

    Deleted .gradle, .idea and build folders

--- src/main/java/sdm/reversi/Main.java
+++ src/main/java/sdm/reversi/Main.java
@@ -11,6 +11,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the game!");
+        System.out.println("What game do you want to play? \n" +
+                "1 - Reversi\n" +
+                "2 - Othello");
+
+        int typeOfGame = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Player1 name: ");
+        String player1Name = scanner.nextLine();
+        System.out.println("Player2 name: ");
+        String player2Name = scanner.nextLine();
+
+        System.out.println("Board size: ");
+        int boardSize = scanner.nextInt();
+
+        Game game;
+        if(typeOfGame == 1){
+            game = new ReversiGame(player1Name,player2Name, new CLIManager(), boardSize);
+        }
+        else{
+            game = new OthelloGame(player1Name,player2Name, new CLIManager(), boardSize);
+        }
+
+        game.play();
 
     }
 }
