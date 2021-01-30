0286f660e4f394390fdc5f3c40cf191f9bc00118
enricodoretto <e.doretto@gmail.com>
2021-01-30 10:00:30 +0100

    Added test to TestReversiPlay: we can now check if a game is over after two moves starting from a given board

--- src/test/java/sdm/reversi/game/TestReversiPlay.java
+++ src/test/java/sdm/reversi/game/TestReversiPlay.java
@@ -16,6 +16,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestReversiPlay {
 
     @Test
+    void isOverAfterTwoMoves() throws IOException, URISyntaxException {
+        URL boardFile = Thread.currentThread().getContextClassLoader().getResource("othello4x4BoardTwoMovesMissing");
+        URL logFile = Thread.currentThread().getContextClassLoader().getResource("gameLog/logOfIsOverAfterTwoMoves");
+        URL inputMoveFile = Thread.currentThread().getContextClassLoader().getResource("gameInputs/movesForIsOverAfterTwoMoves");
+        String messages = Files.readString(Paths.get(logFile.toURI()));
+        System.setIn(inputMoveFile.openStream());
+        ByteArrayOutputStream fakeStandardOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeStandardOutput));
+        Game game = new ReversiGame("Bob", "Alice", boardFile);
+        game.play();
+        assertAll(() -> assertEquals("Bob", game.getWinner().getName()),
+                () -> assertEquals(messages, fakeStandardOutput.toString()));
+    }
+
+    @Test
     void full4x4GameWonByBob() throws URISyntaxException, IOException {
         URL logFile = Thread.currentThread().getContextClassLoader().getResource("gameLog/expectedGameLogReversi4x4");
         URL inputMoveFile = Thread.currentThread().getContextClassLoader().getResource("gameInputs/movesFor4x4ReversiGameWonByBob");
