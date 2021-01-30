a4c59c8b3866b96d5504ff1167a4f8c12e5d611c
enricodoretto <e.doretto@gmail.com>
2021-01-30 09:46:28 +0100

    Refactored TestReversiInitial: renamed method fourMovesCanBeMadeInCentralSquare

--- src/test/java/sdm/reversi/game/TestReversiInitial.java
+++ src/test/java/sdm/reversi/game/TestReversiInitial.java
@@ -50,7 +50,7 @@ public class TestReversiInitial {
 
     @ParameterizedTest
     @CsvSource({"8, 4D-4E-5D-5E-q-", "4, 2B-2C-3B-3C-q-", "10, 5E-5F-6E-6F-q-"})
-    void fourMovesCanBeMadeInCentralSquareParam(int boardSize, String gameSequence) {
+    void fourMovesCanBeMadeInCentralSquare(int boardSize, String gameSequence) {
         ByteArrayInputStream bais = new ByteArrayInputStream(gameSequence.replace("-",System.lineSeparator()).getBytes());
         System.setIn(bais);
         Game game = new ReversiGame("Bob", "Alice", boardSize);
