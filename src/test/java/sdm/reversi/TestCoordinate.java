458e22e2da5c85292730ff272b1995096b43812d
enricodoretto <e.doretto@gmail.com>
2021-01-30 08:59:02 +0100

    Added tests to check if an input coordinate with row > 9 is valid

--- src/test/java/sdm/reversi/TestCoordinate.java
+++ src/test/java/sdm/reversi/TestCoordinate.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCoordinate {
     @ParameterizedTest
-    @CsvSource({"1, A, 0, 0", "2, D, 1, 3", "3, C, 2, 2"})
+    @CsvSource({"1, A, 0, 0", "2, D, 1, 3", "3, C, 2, 2" , "28,Z,27,25"})
     public void hasSameRowAndColumnAsCreated(int row, char column, int boardRow, int boardColumn){
         Coordinate coordinate = new Coordinate(row, column);
         assertAll(
@@ -17,7 +17,7 @@ public class TestCoordinate {
     }
 
     @ParameterizedTest
-    @CsvSource({"1,A,1A", "5,E,5E", "3,D,3D", "8,H,8H"})
+    @CsvSource({"1,A,1A", "5,E,5E", "3,D,3D", "8,H,8H", "28,H,28H", "29,D,29D"})
     void ifAValidInputStringIsValidCoordinate(int row, char column, String inputCoordinate){
         Coordinate coordinate = new Coordinate(row, column);
         assertEquals(coordinate, new Coordinate(inputCoordinate));
