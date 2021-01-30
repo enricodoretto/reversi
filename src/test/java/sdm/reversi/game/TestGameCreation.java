f3b4c2995e4b1ac40d4217a768133f4c9a8f0648
enricodoretto <e.doretto@gmail.com>
2021-01-30 09:28:29 +0100

    Refactored TestGameCreation: removed unused imports

--- src/test/java/sdm/reversi/game/TestGameCreation.java
+++ src/test/java/sdm/reversi/game/TestGameCreation.java
@@ -1,12 +1,11 @@
 package sdm.reversi.game;
 
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
 import sdm.reversi.Disk;
 import sdm.reversi.Player;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class TestGameCreation {
 
@@ -16,7 +15,7 @@ public class TestGameCreation {
         Game othelloGame = new OthelloGame(player1Name, player2Name);
         Game reversiGame = new ReversiGame(player1Name, player2Name);
         assertAll(
-                () -> Assertions.assertEquals(new Player(player1Name, Disk.Color.BLACK), othelloGame.getPlayer1()),
+                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), othelloGame.getPlayer1()),
                 () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), othelloGame.getPlayer2()),
                 () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), reversiGame.getPlayer1()),
                 () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), reversiGame.getPlayer2())
