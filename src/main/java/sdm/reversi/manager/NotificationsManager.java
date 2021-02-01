bd5d8954b4a43e193726c3446a77ccf0a7d1715d
Adriano Tumino <adrianotumino@gmail.com>
2021-02-01 10:15:33 +0100

    Added compose method in NotificationsManager

--- src/main/java/sdm/reversi/manager/NotificationsManager.java
+++ src/main/java/sdm/reversi/manager/NotificationsManager.java
@@ -10,4 +10,42 @@ public interface NotificationsManager {
     void skipTurn();
     void updateGame(Game game);
     void showWinner(Player player);
+
+    default NotificationsManager compose(NotificationsManager other) {
+        if (this.equals(other)) {
+            return this;
+        }
+        NotificationsManager notificationsManager = this;
+        return new NotificationsManager() {
+            @Override
+            public void updateGame(Game game) {
+                notificationsManager.updateGame(game);
+                other.updateGame(game);
+            }
+
+            @Override
+            public void startTurn(Player currentPlayer) {
+                notificationsManager.startTurn(currentPlayer);
+                other.startTurn(currentPlayer);
+            }
+
+            @Override
+            public void skipTurn() {
+                notificationsManager.skipTurn();
+                other.skipTurn();
+            }
+
+            @Override
+            public void initialize(Game game) {
+                notificationsManager.initialize(game);
+                other.initialize(game);
+            }
+
+            @Override
+            public void showWinner(Player player) {
+                notificationsManager.showWinner(player);
+                other.showWinner(player);
+            }
+        };
+    }
 }
