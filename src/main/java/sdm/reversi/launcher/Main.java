a3fa15f6d7ada73386c49eea12a3572c8b239443
Davide Michielin <davidemichie@gmail.com>
2021-02-02 10:18:38 +0100

    swap if else condition in main

--- src/main/java/sdm/reversi/launcher/Main.java
+++ src/main/java/sdm/reversi/launcher/Main.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        if (args != null && args.length > 0 && args[0].equalsIgnoreCase("GUI")) {
-            GUILauncher.launch();
-        } else {
+        if (args != null && args.length > 0 && args[0].equalsIgnoreCase("CLI")) {
             System.out.println("Welcome to the game!");
             CLILauncher.launch();
+        } else {
+            GUILauncher.launch();
         }
     }
 }
