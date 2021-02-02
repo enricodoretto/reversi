5c1c16924ecbec01dee89c0c8f157fd5a4d7158e
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2021-02-02 10:03:38 +0100

    Main method updated: CLI/GUI parameter passed as command line argument

--- src/main/java/sdm/reversi/launcher/GUILauncher.java
+++ src/main/java/sdm/reversi/launcher/GUILauncher.java
@@ -1,4 +1,8 @@
 package sdm.reversi.launcher;
 
 public class GUILauncher {
+
+    public static void launch(){
+        System.out.println("Not ready yet");
+    }
 }
--- src/main/java/sdm/reversi/launcher/Main.java
+++ src/main/java/sdm/reversi/launcher/Main.java
@@ -1,19 +1,15 @@
 package sdm.reversi.launcher;
 
-import sdm.reversi.game.Game;
-import sdm.reversi.game.OthelloGame;
-import sdm.reversi.game.ReversiGame;
-import sdm.reversi.manager.CLIManager;
-import sdm.reversi.manager.GUIManager;
-
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Welcome to the game!");
-        CLILauncher.launch();
-        //Game game = Game.GameBuilder.CLIGameBuilder("Bob").withRemoteOpponent().buildReversi();
-        //game.play();
+        if (args != null && args.length > 0 && args[0].equalsIgnoreCase("GUI")) {
+            GUILauncher.launch();
+        } else {
+            System.out.println("Welcome to the game!");
+            CLILauncher.launch();
+        }
     }
 }
+
