e27ee859e38108eb6cb575938110007b8cba1748
enricodoretto <e.doretto@gmail.com>
2021-02-03 09:54:08 +0100

    Added test to check that disk has same graphical color as disk color

--- src/test/java/sdm/reversi/TestDisk.java
+++ src/test/java/sdm/reversi/TestDisk.java
@@ -4,20 +4,29 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestDisk {
 
     @ParameterizedTest
     @EnumSource(Disk.Color.class)
-    public void hasOriginalSideUpWithNoMoves(Disk.Color color){
+    void hasOriginalSideUpWithNoMoves(Disk.Color color){
         Disk disk = new Disk(color);
         assertEquals(color, disk.getSideUp());
     }
 
     @ParameterizedTest
+    @CsvSource({"BLACK,#000000", "WHITE,#FFFFFF"})
+    void hasSameGraphicalColorAsDiskColor(Disk.Color diskColor, String graphicalColor){
+        Disk disk = new Disk(diskColor);
+        assertEquals(Color.decode(graphicalColor), disk.getSideUp().getGraphicalColor());
+    }
+
+    @ParameterizedTest
     @CsvSource({"BLACK, WHITE", "WHITE, BLACK"})
-    public void changesColorWhenFlipped(Disk.Color initialColor, Disk.Color flippedColor){
+    void changesColorWhenFlipped(Disk.Color initialColor, Disk.Color flippedColor){
         Disk disk = new Disk(initialColor);
         disk.flip();
         assertEquals(flippedColor, disk.getSideUp());
@@ -25,7 +34,7 @@ public class TestDisk {
 
     @ParameterizedTest
     @CsvSource({"BLACK, B", "WHITE, W"})
-    public void isRepresentedAsInitialLetterOfItsSideUpColor(Disk.Color color, String representation){
+    void isRepresentedAsInitialLetterOfItsSideUpColor(Disk.Color color, String representation){
         Disk disk = new Disk(color);
         assertEquals(representation, disk.toString());
     }
