80cda74d0e99d21022cf48b6bbc63728082e850b
Davide Michielin <davidemichie@gmail.com>
2021-02-03 17:31:55 +0100

    added test in TestPlayer for suggested moves on CLI

--- src/test/java/sdm/reversi/TestPlayer.java
+++ src/test/java/sdm/reversi/TestPlayer.java
@@ -1,8 +1,15 @@
 package sdm.reversi;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import sdm.reversi.player.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,4 +25,15 @@ public class TestPlayer {
         );
     }
 
+    @ParameterizedTest
+    @CsvSource({"3A 5D 6H", "4D 6G 7H", "2A 4F 5C"})
+    void withCLIManagerDisplaysSuggestedMovesOnCLI(String suggestedMovesOnCLI) {
+        Player player = new Player("Bob", Disk.Color.BLACK);
+        ByteArrayOutputStream fakeStandardOutput = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(fakeStandardOutput));
+        Set<Coordinate> suggestedMoves = Arrays.stream(suggestedMovesOnCLI.split(" "))
+                .map(Coordinate::new).collect(Collectors.toSet());
+        player.suggestMoves(suggestedMoves);
+        assertEquals(suggestedMovesOnCLI+System.lineSeparator(), fakeStandardOutput.toString());
+    }
 }
