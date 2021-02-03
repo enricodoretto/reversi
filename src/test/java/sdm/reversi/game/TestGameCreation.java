a55337cff0ac48e73b4e4b04347b0f4276285e73
Davide Michielin <davidemichie@gmail.com>
2021-02-03 18:18:47 +0100

    Refactored tests in TestGameCreation

--- src/test/java/sdm/reversi/game/TestGameCreation.java
+++ src/test/java/sdm/reversi/game/TestGameCreation.java
@@ -12,14 +12,20 @@ public class TestGameCreation {
 
     @ParameterizedTest
     @CsvSource({"Bob, Alice", "Jack, John", "Simon, Leonard"})
-    void withTwoPlayersWithDifferentNamesAndDefaultBoardSucceeds(String player1Name, String player2Name) {
-        Game othelloGame = Game.GameBuilder.CLIGameBuilder(player1Name).withOpponent(player2Name).withBoardSize(8).buildOthello();
-        Game reversiGame = Game.GameBuilder.CLIGameBuilder(player1Name).withOpponent(player2Name).withBoardSize(8).buildReversi();
+    void withTwoPlayersWithDifferentNamesAndBoardSize8Succeeds(String player1Name, String player2Name) {
+        Game othelloGameCLI = Game.GameBuilder.CLIGameBuilder(player1Name).withOpponent(player2Name).withBoardSize(8).buildOthello();
+        Game reversiGameCLI = Game.GameBuilder.CLIGameBuilder(player1Name).withOpponent(player2Name).withBoardSize(8).buildReversi();
+        Game othelloGameGUI = Game.GameBuilder.CLIGameBuilder(player1Name).withOpponent(player2Name).withBoardSize(8).buildOthello();
+        Game reversiGameGUI = Game.GameBuilder.CLIGameBuilder(player1Name).withOpponent(player2Name).withBoardSize(8).buildReversi();
         assertAll(
-                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), othelloGame.getPlayer1()),
-                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), othelloGame.getPlayer2()),
-                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), reversiGame.getPlayer1()),
-                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), reversiGame.getPlayer2())
+                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), othelloGameCLI.getPlayer1()),
+                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), othelloGameCLI.getPlayer2()),
+                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), reversiGameCLI.getPlayer1()),
+                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), reversiGameCLI.getPlayer2()),
+                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), othelloGameGUI.getPlayer1()),
+                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), othelloGameGUI.getPlayer2()),
+                () -> assertEquals(new Player(player1Name, Disk.Color.BLACK), reversiGameGUI.getPlayer1()),
+                () -> assertEquals(new Player(player2Name, Disk.Color.WHITE), reversiGameGUI.getPlayer2())
         );
     }
 
@@ -28,7 +34,9 @@ public class TestGameCreation {
     void failsWithBothPlayersWithSameName(String playerName) {
         assertAll(
                 () -> assertThrows(IllegalArgumentException.class, () -> Game.GameBuilder.CLIGameBuilder(playerName).withOpponent(playerName).withBoardSize(8).buildOthello()),
-                () -> assertThrows(IllegalArgumentException.class, () -> Game.GameBuilder.CLIGameBuilder(playerName).withOpponent(playerName).withBoardSize(8).buildReversi())
+                () -> assertThrows(IllegalArgumentException.class, () -> Game.GameBuilder.CLIGameBuilder(playerName).withOpponent(playerName).withBoardSize(8).buildReversi()),
+                () -> assertThrows(IllegalArgumentException.class, () -> Game.GameBuilder.GUIGameBuilder(playerName).withOpponent(playerName).withBoardSize(8).buildOthello()),
+                () -> assertThrows(IllegalArgumentException.class, () -> Game.GameBuilder.GUIGameBuilder(playerName).withOpponent(playerName).withBoardSize(8).buildReversi())
         );
     }
 }
