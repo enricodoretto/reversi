2b03b50038d216f2f9940989320a6f62c4af3748
enricodoretto <e.doretto@gmail.com>
2021-02-03 12:01:15 +0100

    Refactored TestCoordinate: coordinate is now created with constructor Coordinate(int, int)

--- src/test/java/sdm/reversi/TestCoordinate.java
+++ src/test/java/sdm/reversi/TestCoordinate.java
@@ -7,18 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCoordinate {
     @ParameterizedTest
-    @CsvSource({"1, A, 0, 0", "2, D, 1, 3", "3, C, 2, 2", "28,Z,27,25"})
-    public void hasSameRowAndColumnAsCreated(int row, char column, int boardRow, int boardColumn){
+    @CsvSource({"0, 0", "1, 3", "2, 2", "27,25"})
+    public void hasSameRowAndColumnAsCreated(int row, int column){
         Coordinate coordinate = new Coordinate(row, column);
         assertAll(
-                () -> assertEquals(boardRow, coordinate.getRow()),
-                () -> assertEquals(boardColumn, coordinate.getColumn())
+                () -> assertEquals(row, coordinate.getRow()),
+                () -> assertEquals(column, coordinate.getColumn())
         );
     }
 
     @ParameterizedTest
-    @CsvSource({"1,A,1A", "5,E,5E", "3,D,3D", "8,H,8H", "28,H,28H", "29,D,29D"})
-    void ifAValidInputStringIsValidCoordinate(int row, char column, String inputCoordinate){
+    @CsvSource({"0,0,1A", "4,4,5E", "2,3,3D", "7,7,8H", "27,7,28H", "28,3,29D", "26,26,27AA","30,30,31AE","99,0,100A"})
+    void ifAValidInputStringIsValidCoordinate(int row, int column, String inputCoordinate){
         Coordinate coordinate = new Coordinate(row, column);
         assertEquals(coordinate, new Coordinate(inputCoordinate));
     }
@@ -30,16 +30,9 @@ public class TestCoordinate {
     }
 
     @ParameterizedTest
-    @CsvSource({"26,26,27AA","30,30,31AE","99,0,100A"})
-    void ifInputStringIsValidCoordinate(int row, int column, String inputCoordinate){
-        Coordinate coordinate = new Coordinate(row, column);
-        assertEquals(coordinate, new Coordinate(inputCoordinate));
-    }
-
-    @ParameterizedTest
     @CsvSource({"279ABBA","31AE","100A"})
-    void isPrintedCorrectly(String inputCoordinate){
-        assertEquals(inputCoordinate, new Coordinate(inputCoordinate).toString());
+    void isCorrectlyRepresentedAsString(String coordinate){
+        assertEquals(coordinate, new Coordinate(coordinate).toString());
     }
 
 }
